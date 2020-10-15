29bac3a39775cc7343e8d340465c324b84f0929a
Steve Krueger <stevekrueger52@gmail.com>
2020-10-15 16:33:14 -0400

    Added Scarlett's main character as a prefab (not yet in main scene)

---
+++ Assets/_Eigengrau/Scripts/Figure.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Figure : MonoBehaviour
+{
+    [SerializeField]
+    public float moveSpeed;
+
+    private float xInput;
+    private Animator playerAction;
+
+    private void Start()
+    {
+        this.playerAction = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (xInput >= 1)
+        {
+            playerAction.SetBool("isWalkingRight", true);
+            playerAction.SetBool("isWalkingLeft", false);
+        }
+        else if (xInput <= -1)
+        {
+            playerAction.SetBool("isWalkingRight", false);
+            playerAction.SetBool("isWalkingLeft", true);
+        }
+        else
+        {
+            playerAction.SetBool("isWalkingRight", false);
+            playerAction.SetBool("isWalkingLeft", false);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        this.xInput = Input.GetAxisRaw("Horizontal");
+        transform.Translate(new Vector2(xInput, 0) * moveSpeed * Time.deltaTime);
+    }
+}
