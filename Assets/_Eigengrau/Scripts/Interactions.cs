f006f2067065f0aa758caf5fd4a34462ad866e79
Karina Sinha <ksinha1224@gmail.com>
2020-11-19 17:41:56 -0500

    Tried to get Interactions working

---
+++ Assets/_Eigengrau/Scripts/Interactions.cs
@@ -0,0 +1,183 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Interactions : MonoBehaviour
+{
+    string text = "";
+    GameObject txtObj;
+    int textState; //0 = no text displayed, 1 = text animating, 2 = text done, waiting for click to proceed, 3 = door switch
+    public GameObject chara;
+    bool doorSwitch;
+    int changingScene; //0 = no choice selected, 1 = changing scene, 2 = not changing scene
+    GameObject yesBtn;
+    GameObject noBtn;
+
+    private void Start()
+    {
+        txtObj = this.gameObject.transform.GetChild(1).gameObject;
+        yesBtn = txtObj.transform.GetChild(0).gameObject;
+        noBtn = txtObj.transform.GetChild(1).gameObject;
+        yesBtn.SetActive(false);
+        noBtn.SetActive(false);
+        this.gameObject.SetActive(false);
+        textState = 0;
+        doorSwitch = false;
+        changingScene = 0;
+    }
+
+    private void Update()
+    {
+        if(textState != 0) //disable character movement if they're interacting with something
+        {
+            this.gameObject.SetActive(true);
+            chara.GetComponent<Figure>().moveSpeed = 0;
+        }
+        else
+        {
+            this.gameObject.SetActive(false);
+            chara.GetComponent<Figure>().moveSpeed = 5;
+        }
+
+        if (textState == 2)
+        {            
+            StartCoroutine(WaitForClick());
+        }
+
+        if (textState == 3)
+        {
+            yesBtn.SetActive(true);
+            noBtn.SetActive(true);
+            StartCoroutine(SceneChangeCheck());
+        }
+    }
+
+    public void setTextBed()
+    {
+        Debug.Log("aaa???");
+        if(textState == 0)
+        {
+            text = "It's not time to sleep yet...";
+            displayFlavorText();
+        }
+    }
+
+    public void setTextPainting()
+    {
+        if (textState == 0)
+        {
+            text = "A true classic! How did I get it out of the museum again...?";
+            displayFlavorText();
+        }            
+    }
+
+    public void setTextDesk()
+    {
+        if (textState == 0)
+        {
+            text = "My desk is kind of cluttered, oops.";
+            displayFlavorText();
+        }            
+    }
+
+    public void setTextWallpaper()
+    {
+        if (textState == 0)
+        {
+            text = "This wallpaper really livens up my room! I wonder if I can make it better...";
+            displayFlavorText();
+        }            
+    }
+
+    public void setTextWindow()
+    {
+        if (textState == 0)
+        {
+            text = "The view from my apartment is great! I can see everything I want!";
+            displayFlavorText();
+        }            
+    }
+
+    public void setTextLamp()
+    {
+        if (textState == 0)
+        {
+            text = "*Click* Light on! *Click* Light off. *Click* Light on! *Click* Light off.";
+            displayFlavorText();
+        }            
+    }
+
+    public void interactDoor()
+    {
+        if (textState == 0)
+        {
+            text = "Should I head out?";
+            doorSwitch = true;
+            StartCoroutine(AnimateText());
+        }
+    }
+
+    void displayFlavorText()
+    {        
+        textState = 1;
+        StartCoroutine(AnimateText());
+    }
+
+    IEnumerator AnimateText()
+    {
+        for (int i = 0; i < text.Length; i++)
+        {
+            txtObj.GetComponent<Text>().text = text.Substring(0, i);
+            yield return new WaitForSeconds(0.03f);
+        }
+
+        if(doorSwitch)
+        {
+            textState = 3;
+        }
+
+        else
+        {
+            textState = 2;
+        }
+        
+        yield return null;
+    }
+
+    IEnumerator WaitForClick()
+    {
+        if(Input.GetMouseButton(0))
+        {
+            textState = 0;
+            yield break;
+        }
+    }
+
+    IEnumerator SceneChangeCheck()
+    {
+        while (changingScene == 0)
+        {
+            yield return null;
+        }
+    }
+
+    public void confirmChangeScene()
+    {
+        changingScene = 1;
+        //SceneManager.LoadScene(streetscene);
+        Debug.Log("Changing To Street Scene... jk that doesn't exist yet :P");
+        textState = 0;
+    }
+
+    public void notChangingScene()
+    {
+        changingScene = 2;
+        textState = 0;
+        yesBtn.SetActive(false);
+        noBtn.SetActive(false);
+        doorSwitch = false;
+    }
+
+}
